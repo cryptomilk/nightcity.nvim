01899c6447290decb3af37286ce11f800d67201d
Andreas Schneider <asn@cryptomilk.org>
2023-04-26 17:34:10 +0200

    feat(extras): Add a C example

---
+++ extras/showcase/lang.c
@@ -0,0 +1,54 @@
+#include <stdint.h>
+#include <stdio.h>
+
+struct foobar {
+  uint8_t wurst;
+  void *ptr;
+};
+enum scope {
+    FILE_SCOPE1,
+    FILE_SCOPE2
+};
+
+extern int globalScope;
+static enum scope fileScope;
+
+void wurst(struct foobar *parameter) {
+  int functionScope = 3;
+  const char *wurst = "brot";
+
+  globalScope = 42;
+  fileScope = 32;
+
+  /* NOTE this is bug #123 */
+  functionScope = 0.1;
+
+  /* TODO */
+  switch (fileScope) {
+  case FILE_SCOPE1:
+    break;
+  default:
+    return 5;
+  }
+
+  parameter->ptr = NULL;
+
+/* clangd marks unused code as comment */
+#if 0
+  /* TODO FIXME XXX wurst */
+
+  str = "This is commented out"
+#endif
+
+  return;
+}
+
+int main(int argc, char *argv[]) {
+  struct foobar s = {
+      .wurst = '0',
+  };
+
+  wurst(&s);
+
+  return 0;
+}
